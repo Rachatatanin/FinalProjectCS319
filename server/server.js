8bb69dabd23637af73317669e1ec61b99dde793b
rachata.tani <rachata.tani@bumail.net>
2023-12-03 17:17:18 +0700

    basestart

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+app.get('/api', (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]});
+})
+
+mongoose.connect('mongodb+srv://rachatatani:SMfrdecbooHrcmfq@cluster0.46insu0.mongodb.net/?retryWrites=true&w=majority', )
+    .then(() => console.log('connection successfully'))
+    .catch(err => console.log(err));
+
+app.listen(5000, () => {console.log('Server running on port 5000')});
